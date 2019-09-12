1d4bafc642affece9cbc4503a6626c35228f5b29
Daniel Mendoza <dmendoza@fb.com>
2019-09-12 14:12:58 -0700

    Updates

---
+++ precache-manifest.cd6d345d6697ecac19dab4f726a9779a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7b2adc388b6d8df29d7e412e17d41e",
+    "url": "/movie-browser/index.html"
+  },
+  {
+    "revision": "492e89f38f998f30445e",
+    "url": "/movie-browser/static/css/main.f31aca0e.chunk.css"
+  },
+  {
+    "revision": "86a8b0e79e3a204fa6df",
+    "url": "/movie-browser/static/js/2.4e2320f2.chunk.js"
+  },
+  {
+    "revision": "492e89f38f998f30445e",
+    "url": "/movie-browser/static/js/main.59cdab66.chunk.js"
+  },
+  {
+    "revision": "4aa5c5b36ef920583e3b",
+    "url": "/movie-browser/static/js/runtime~main.6d896914.js"
+  },
+  {
+    "revision": "9ba7940a2d86de1760c73628b4ff02cd",
+    "url": "/movie-browser/static/media/movie-logo.9ba7940a.jpg"
+  }
+]);
